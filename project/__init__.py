1dd72ffba916284d070b83222290e8d31254b5d1
bashhack (Marc Laughton) <info@marclaughton.com>
2020-01-18 21:07:07 -0500

    Wire up PostgreSQL to Flask app + create User model

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,11 @@
-from flask import Flask, jsonify
-from flask_restplus import Resource, Api
+import os
+
+from flask import Flask
+from flask_restplus import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy.model import Model
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import Boolean, Integer, String
 
 
 # instantiate the app
@@ -7,7 +13,23 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object("project.config.DevelopmentConfig")
+app_settings = os.getenv("APP_SETTINGS")
+app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+# model
+class User(Model):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(128), nullable=False)
+    email = Column(String(128), nullable=False)
+    active = Column(Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class Ping(Resource):
