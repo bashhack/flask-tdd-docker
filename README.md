381cfa79c41dc012c70c3403350377227704b689
bashhack (Marc Laughton) <info@marclaughton.com>
2020-01-19 18:57:12 -0500

    Add Gitlab Pipeline CI status badge + remove secret_key test asserts

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test-Driven Development with Python, Flask, and Docker
+
+[![pipeline status](https://gitlab.com/bashhack/flask-tdd-docker/badges/master/pipeline.svg)](https://gitlab.com/bashhack/flask-tdd-docker/commits/master)
--- project/tests/unit/test_config.py
+++ project/tests/unit/test_config.py
@@ -7,7 +7,6 @@ def test_postgres_host(test_app):
 
 def test_development_config(test_app):
     test_app.config.from_object("project.config.DevelopmentConfig")
-    assert test_app.config["SECRET_KEY"] == "my_precious"
     assert test_app.config["POSTGRES_HOST"] == os.environ.get("POSTGRES_HOST")
     assert not test_app.config["TESTING"]
     assert test_app.config["SQLALCHEMY_DATABASE_URI"] == os.environ.get("DATABASE_URL")
@@ -15,7 +14,6 @@ def test_development_config(test_app):
 
 def test_testing_config(test_app):
     test_app.config.from_object("project.config.TestingConfig")
-    assert test_app.config["SECRET_KEY"] == "my_precious"
     assert test_app.config["POSTGRES_HOST"] == os.environ.get("POSTGRES_HOST")
     assert test_app.config["TESTING"]
     assert test_app.config["SQLALCHEMY_DATABASE_URI"] == os.environ.get(
@@ -25,7 +23,6 @@ def test_testing_config(test_app):
 
 def test_production_config(test_app):
     test_app.config.from_object("project.config.ProductionConfig")
-    assert test_app.config["SECRET_KEY"] == "my_precious"
     assert test_app.config["POSTGRES_HOST"] == os.environ.get("POSTGRES_HOST")
     assert not test_app.config["TESTING"]
     assert test_app.config["SQLALCHEMY_DATABASE_URI"] == os.environ.get("DATABASE_URL")
