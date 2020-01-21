36f384b96154b601734d27fa3dcbb7ed2f24e4ff
bashhack (Marc Laughton) <info@marclaughton.com>
2020-01-20 21:06:15 -0500

    Create initial React UsersList component + tests + CORS config to allow React / Flask communication

---
+++ services/client/src/components/__tests__/UsersList.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+
+import UsersList from '../UsersList';
+
+afterEach(cleanup);
+
+const users = [
+    {
+        'email': 'testuser1@example.com',
+        'id': 1,
+        'username': 'testuser1'
+    },
+    {
+        'email': 'testuser2@example.com',
+        'id': 2,
+        'username': 'testuser2'
+    }
+];
+
+it('renders a username', () => {
+    const { getByText } = render(<UsersList users={users}/>);
+    expect(getByText('testuser1')).toHaveClass('username');
+    expect(getByText('testuser2')).toHaveClass('username');
+});
--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import UsersList from './components/UsersList';
 
-const App = () => {
-    return (
+class App extends Component {
+    constructor() {
+      super();
+
+      this.state = {
+        users: []
+      };
+    }
+
+    componentDidMount() {
+      this.getUsers();
+    }
+
+    getUsers() {
+      axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+        .then((res) => { this.setState({ users: res.data }); })
+        .then((err) => { console.log(err); });
+    }
+
+    render() {
+      return (
         <section className="section">
-            <div className="container">
-                <div className="columns">
-                    <div className="column is-one-third">
-                        <br/>
-                        <h1 className="title is-1 is-1">Users</h1>
-                        <hr/><br/>
-                    </div>
-                </div>
+          <div className="container">
+            <div className="columns">
+              <div className="column is-one-third">
+                <br/>
+                <h1 className="title is-1 is-1">Users</h1>
+                <hr/><br/>
+                <UsersList users={this.state.users}/>
+              </div>
             </div>
+          </div>
         </section>
-    );
+      );
+    }
 };
 
 ReactDOM.render(
