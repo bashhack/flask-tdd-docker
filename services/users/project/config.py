a0d55c6f75b46a271895a4b3ea966cb797cc21ab
bashhack (Marc Laughton) <info@marclaughton.com>
2020-01-22 23:37:12 -0500

    Add jwt timing expirations

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -7,6 +7,8 @@ class BaseConfig:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
     BCRYPT_LOG_ROUNDS = 13
+    ACCESS_TOKEN_EXPIRATION = 900  # 15 minutes
+    REFRESH_TOKEN_EXPIRATION = 2592000  # 30 days
 
 
 class DevelopmentConfig(BaseConfig):
@@ -18,6 +20,8 @@ class TestingConfig(BaseConfig):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
     BCRYPT_LOG_ROUNDS = 4
+    ACCESS_TOKEN_EXPIRATION = 3
+    REFRESH_TOKEN_EXPIRATION = 3
 
 
 class ProductionConfig(BaseConfig):
