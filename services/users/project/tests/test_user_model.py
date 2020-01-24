8b9665bd2fa506ea3b93b530366d10a8686d28f4
bashhack (Marc Laughton) <info@marclaughton.com>
2020-01-23 21:50:12 -0500

    Add tests for JWT exp + remove fake secret_key to CI/CI pipeline in Gitlab

--- services/users/project/tests/test_user_model.py
+++ services/users/project/tests/test_user_model.py
@@ -1,3 +1,8 @@
+import time
+from datetime import datetime
+
+import jwt
+
 from project.api.users.models import User
 
 
@@ -18,3 +23,25 @@ def test_decode_token(test_app, test_database, add_user):
     token = user.encode_token(user.id, "access")
     assert isinstance(token, bytes)
     assert User.decode_token(token) == user.id
+
+
+def test_payload_expiration_for_token_type_access(
+    test_app, test_database, add_user, freezer
+):
+    user = add_user("foo", "bar@baz.com", "foobar")
+    token = user.encode_token(user.id, "access")
+    payload = jwt.decode(token, test_app.config.get("SECRET_KEY"))
+    assert payload["exp"] is not None
+    now = int(time.mktime(datetime.utcnow().timetuple()))
+    assert payload["exp"] - now == test_app.config["ACCESS_TOKEN_EXPIRATION"]
+
+
+def test_payload_expiration_for_token_type_non_access(
+    test_app, test_database, add_user, freezer
+):
+    user = add_user("foo", "bar@baz.com", "foobar")
+    token = user.encode_token(user.id, "other_token_type")
+    payload = jwt.decode(token, test_app.config.get("SECRET_KEY"))
+    assert payload["exp"] is not None
+    now = int(time.mktime(datetime.utcnow().timetuple()))
+    assert payload["exp"] - now == test_app.config["REFRESH_TOKEN_EXPIRATION"]
