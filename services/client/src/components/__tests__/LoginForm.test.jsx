43eb0d4083d33afd507f68883b9a41288931d160
bashhack (Marc Laughton) <info@marclaughton.com>
2020-01-26 16:29:01 -0500

    Update tests for Formik/Yup interaction + validation for Login/Register

--- services/client/src/components/__tests__/LoginForm.test.jsx
+++ services/client/src/components/__tests__/LoginForm.test.jsx
@@ -5,12 +5,33 @@ import LoginForm from "../LoginForm";
 
 afterEach(cleanup);
 
+const props = {
+  handleLoginFormSubmit: () => {
+    return true;
+  }
+};
+
 it("renders", () => {
-  const { asFragment } = render(<LoginForm />);
+  const { asFragment } = render(<LoginForm {...props} />);
   expect(asFragment()).toMatchSnapshot();
 });
 
 it("renders properly", () => {
-  const { getByText } = render(<LoginForm />);
+  const { getByText } = render(<LoginForm {...props} />);
   expect(getByText("Log In")).toHaveClass("title");
+});
+
+it("renders with default propers", () => {
+  const { getByLabelText, getByText } = render(<LoginForm {...props} />);
+
+  const emailInput = getByLabelText("Email");
+  expect(emailInput).toHaveAttribute("type", "email");
+  expect(emailInput).not.toHaveValue();
+
+  const passwordInput = getByLabelText("Password");
+  expect(passwordInput).toHaveAttribute("type", "password");
+  expect(passwordInput).not.toHaveValue();
+
+  const buttonInput = getByText("Submit");
+  expect(buttonInput).toHaveValue("Submit");
 });
